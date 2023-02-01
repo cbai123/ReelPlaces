b795ed0922f3ef56d54fc27a5590abe74e7be4c5
Magdaline <mamoakohene@gmail.com>
2023-02-01 02:19:47 +0000

    created listing function for app with an example API - not tested

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [isLoading, setIsLoading] = useState(true);
+  let [response, setResponse] = useState();
+
+  useEffect(() => {
+    fetch("https://official-joke-api.appspot.com/random_ten")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log("I " + result)
+          setResponse(result);
+          console.log("II " + response);
+
+          console.log("III " + result)
+          console.log(result.id);
+          setIsLoading(false);
+        }
+    )
+  }, []);
+
+  const getListing = () => {
+    console.log(isLoading)
+    if (isLoading) {
+      return <ActivityIndicator size="large" />;
+    }
+    console.log("IV " + response )
+
+    //return <Text>{ response[0].setup }</Text>
+
+    return response.map((movie) => (<Text> Movie:{movie.setup} Location: {movie.punchline}</Text>
+    ))
+  }
+
+  //function render (){ 
+    return (
+      <View style={styles.container}>
+        <Text>ReelPlaces</Text>
+        {getListing()}
+        <StatusBar style="auto" />
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
