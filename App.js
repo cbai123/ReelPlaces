2d506f205d61dcacd960ab661ebd4f8acaa69e38
Chris <c.bailey00@hotmail.com>
2023-02-02 12:03:09 +0000

    trying to setup ui-kitten

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+// import { Text, View } from 'react-native';
 import Search from './components/search';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Search />
+   
+     <ApplicationProvider {...eva} theme={eva.dark}>
+      {/* <View style={styles.container}> */}
+      {/* <View> */}
+        <Text>Open up App.js to start working on your app!</Text>
+        {/* <StatusBar style="auto" /> */}
+        <Search />
 
-    </View>
+      {/* </View> */}
+     </ApplicationProvider>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
